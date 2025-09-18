5a1649b390b6ea3bba6e9df0566397ec49aa3f1e
Felipe Venas <felipevenas3003@gmail.com>
2025-09-18 11:56:15 -0300

    Outras alterações...

---
+++ app/domain/user/services.py
@@ -0,0 +1,18 @@
+from infra.api_client import APIClient
+
+class UserService():
+
+    def find_all():
+        return APIClient.get("/users")
+
+    def find_by_id(user_id: int):
+        return APIClient.get(f"user/{user_id}")
+
+    def create_user(data: dict):
+        return APIClient.post("/user", data=data)
+
+    def update(user_id: int):
+        return APIClient.put(f"/user/{user_id}")
+
+    def delete(user_id: int):
+        return APIClient.delete(f"/user/{user_id}")
---
+++ app/infra/api_client.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv 
+
+import os
+import requests
+
+load_dotenv()
+API_URL = os.getenv('API_URL')
+
+class APIClient:
+
+    @staticmethod
+    def _request(method, endpoint, data=None, params=None):
+        url = f"{API_URL}/{endpoint}"
+        try:
+            response = requests.request(method, url, json=data, params=params)
+            response.raise_for_status()
+            if response.status_code == 204:
+                return None
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"Erro de comunicação com a API em {method} {url}: {e}")        
+
+    @staticmethod
+    def get(endpoint, params=None):
+        return APIClient._request('GET', endpoint, params=params)
+    
+    @staticmethod
+    def post(endpoint, data=None):
+        return APIClient._request('POST', endpoint, data=data)
+    
+    @staticmethod
+    def put(endpoint, data=None):
+        return APIClient._request('PUT', endpoint, data=data)
+    
+    @staticmethod
+    def delete(endpoint, data=None):
+        return APIClient._request('DELETE', endpoint, data=data)
