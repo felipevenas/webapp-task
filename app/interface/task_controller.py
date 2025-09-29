2d86e6a438f2c456467af02fce89dd8112330a59
Felipe Venas <felipevenas3003@gmail.com>
2025-09-29 11:37:19 -0300

    Features: Criação do controlador para a classe Task

---
+++ app/interface/task_controller.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, redirect, url_for, request, flash
+
+from app.forms.forms import CreateTaskForm
+from app.domain.task.services import TaskService
+
+task_bp = Blueprint('task_bp', __name__)
+
+@task_bp.route('/task', methods=['POST'])
+def create_task():
+
+    form = CreateTaskForm(request.form)
+
+    if form.validate_on_submit():
+        response = TaskService.create_task(form)
+
+        if response and response.status_code == 200:
+            flash('Tarefa adicionada com sucesso!', category='sucess')
+            return redirect(url_for("index_bp.tasks_page"))
+        else:
+            flash('Não foi possível adicionar a tarefa. Tente novamente!', category='danger')   
+            return redirect(url_for('index_bp.tasks_page'))
