56d8c0cfcd9a64eec194ce58edbf772bacd1f681
Felipe Venas <felipevenas3003@gmail.com>
2025-09-29 11:42:17 -0300

    Outras alterações básicas

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.interface.controller import index_bp
 from app.interface.user_controller import user_bp
 from app.interface.auth.auth_controller import auth_bp
+from app.interface.task_controller import task_bp
 from dotenv import load_dotenv
 import os
 
@@ -12,4 +13,5 @@ app.secret_key = os.getenv('SECRET_KEY')
 app.register_blueprint(index_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(auth_bp)
+app.register_blueprint(task_bp)
 
